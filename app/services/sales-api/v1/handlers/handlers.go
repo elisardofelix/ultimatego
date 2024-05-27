1051782e5b672d112115df4f2a8be7dc41ef3a1b
Elisardo Felix <elisardofelix@gmail.com>
2024-05-27 14:37:10 -0400

    commit 20

--- app/services/sales-api/v1/handlers/handlers.go
+++ app/services/sales-api/v1/handlers/handlers.go
@@ -1,14 +1,14 @@
 package handlers
 
 import (
-	"github.com/dimfeld/httptreemux/v5"
 	"github.com/elisardofelix/ultimatego/app/services/sales-api/v1/handlers/hackgrp"
 	v1 "github.com/elisardofelix/ultimatego/business/web/v1"
+	"github.com/elisardofelix/ultimatego/foundation/web"
 )
 
 type Routes struct{}
 
 // Add implements the RouterAdder interface.
-func (Routes) Add(mux *httptreemux.ContextMux, cfg v1.APIMuxConfig) {
-	hackgrp.Routes(mux)
+func (Routes) Add(app *web.App, cfg v1.APIMuxConfig) {
+	hackgrp.Routes(app)
 }
