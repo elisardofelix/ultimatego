7dba83c32527002ed39b555c95f0f1c56d215301
Elisardo Felix <elisardofelix@gmail.com>
2024-06-10 05:26:55 -0400

    commit 26

--- app/services/sales-api/v1/handlers/hackgrp/hackgrp.go
+++ app/services/sales-api/v1/handlers/hackgrp/hackgrp.go
@@ -2,12 +2,19 @@ package hackgrp
 
 import (
 	"context"
+	"errors"
+	"math/rand"
 	"net/http"
 
+	"github.com/elisardofelix/ultimatego/business/web/v1/response"
 	"github.com/elisardofelix/ultimatego/foundation/web"
 )
 
 func Hack(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	if n := rand.Intn(100) % 2; n == 0 {
+		return response.NewError(errors.New("TRUST ERROR"), http.StatusBadRequest)
+	}
+
 	status := struct {
 		Status string
 	}{
