81052c0e4b4d9024817b69eb0c3973155204efc6
Elisardo Felix <elisardofelix@gmail.com>
2024-07-10 19:37:48 -0400

    commit 40

--- business/web/v1/mid/errors.go
+++ business/web/v1/mid/errors.go
@@ -7,6 +7,7 @@ import (
 	"github.com/elisardofelix/ultimatego/business/web/v1/auth"
 	"github.com/elisardofelix/ultimatego/business/web/v1/response"
 	"github.com/elisardofelix/ultimatego/foundation/logger"
+	"github.com/elisardofelix/ultimatego/foundation/validate"
 	"github.com/elisardofelix/ultimatego/foundation/web"
 )
 
@@ -25,6 +26,17 @@ func Errors(log *logger.Logger) web.Middleware {
 				switch {
 				case response.IsError(err):
 					reqErr := response.GetError(err)
+
+					if validate.IsFieldErrors(reqErr.Err) {
+						fieldErrors := validate.GetFieldErrors(reqErr.Err)
+						er = response.ErrorDocument{
+							Error:  "data validation error",
+							Fields: fieldErrors.Fields(),
+						}
+						status = reqErr.Status
+						break
+					}
+
 					er = response.ErrorDocument{
 						Error: reqErr.Error(),
 					}
